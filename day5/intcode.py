26b294e7e23f1cbdc0dc1ebbf49df4d53a1b6fe9
Jan-Jaap Korpershoek <jjkorpershoek96@gmail.com>
2019-12-05 19:04:01 +0100

    Formatting

--- day5/intcode.py
+++ day5/intcode.py
@@ -1,5 +1,6 @@
 def decode_opcode(opcode):
-    return (opcode % 100, opcode//100)
+    return (opcode % 100, opcode // 100)
+
 
 def get_values(params, modes, memory):
     values = []
@@ -12,6 +13,7 @@ def get_values(params, modes, memory):
             values.append(param)
     return values
 
+
 def computer(memory):
     ip = 0
     while True:
@@ -21,21 +23,20 @@ def computer(memory):
         if opcode == 99:
             return memory
         elif opcode == 3 or opcode == 4:
-            par1 = memory[ip+1]
+            par1 = memory[ip + 1]
             if opcode == 3:
                 memory[par1] = get_input()
             else:
                 print(memory[par1])
             ip += 2
             continue
-        par1, par2 = get_values(memory[ip+1:ip+3], modes, memory)
+        par1, par2 = get_values(memory[ip + 1:ip + 3], modes, memory)
         if opcode == 1 or opcode == 2:
-            dest = memory[ip+3]
+            dest = memory[ip + 3]
             memory[dest] = par1 + par2 if opcode == 1 else par1 * par2
             ip += 4
         elif opcode == 5 or opcode == 6:
-            if par1 != 0 and opcode == 5 \
-                or par1 == 0 and opcode == 6:
+            if par1 != 0 and opcode == 5 or par1 == 0 and opcode == 6:
                 ip = par2
             else:
                 ip += 3
@@ -45,7 +46,7 @@ def computer(memory):
             else:
                 check = par1 == par2
             res = 1 if check else 0
-            memory[memory[ip+3]] = res
+            memory[memory[ip + 3]] = res
             ip += 4
 
 
@@ -54,8 +55,10 @@ def compute(noun, verb, memory):
     memory[2] = verb
     return computer(memory)
 
+
 def convert(string):
     return [int(x) for x in string.split(',')]
+
 
 with open('input', 'r') as f:
     input_string = f.read()
@@ -63,6 +66,8 @@ with open('input', 'r') as f:
 for id in [1, 5]:
     input = convert(input_string)
     print(f"{id=}")
+
     def get_input():
         return id
+
     computer(input)
